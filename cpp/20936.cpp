a9b0b84a17e2f63122078a1e2f92945847818e21
Jaeyong <ggjae_@daum.net>
2021-03-03 17:36:36 +0900

    20936

---
+++ cpp/20936.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    string m;
+    getline(cin,m);
+    for(int i=0;i<m.size();i++){
+        printf("%c",m[i]);
+    }
+    return 0;
+}
