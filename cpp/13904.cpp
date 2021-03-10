0a5e44f05257b16d7272d6342f05b5844669002e
Jaeyong <ggjae_@daum.net>
2021-03-10 20:27:19 +0900

    i ge na y a~

---
+++ cpp/13904.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool isuse[1001];
+bool compare(const pair<int,int> &a, const pair<int, int > &b)
+{
+	if (a.first == b.first) {
+		return a.second > b.second;
+	}
+	return a.first > b.first;
+}
+int main(){
+    int n;
+    vector<pair<int, int> > v;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int d,w;
+        cin >> d >> w;
+        v.push_back(make_pair(w,d));
+    }
+    sort(v.begin(),v.end(),compare);
+    int sum = 0;
+    isuse[0] = true;
+    for(int i=0;i<n;i++){
+        int grade = v[i].first;
+        int daytime = v[i].second;
+        // printf("grade : %d, daytime : %d\n",grade,daytime);
+        if(isuse[daytime] == false){
+            isuse[daytime] = true;
+            sum += grade;
+            // printf("%d 만큼 추가됐음",grade);
+            // printf("-------------------------\n");
+        } else {
+            while(daytime>0){
+                // printf("못들어갔어-\n");
+                daytime--;
+                if(isuse[daytime] == false){
+                    isuse[daytime] = true;
+                    sum += grade;
+                    break;
+                }
+            }
+        }
+    }
+    cout << sum;
+    // 값이 큰놈부터 마감일에다 넣고 생각할까..?
+    return 0;
+}
