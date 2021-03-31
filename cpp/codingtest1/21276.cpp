69de37f9cebd5656fa887d77059e87aac517e8ae
Jaeyong <ggjae_@daum.net>
2021-04-01 01:40:25 +0900

    다시 풀어야 할 것 #1

--- cpp/codingtest1/21276.cpp
+++ cpp/codingtest1/21276.cpp
@@ -1,41 +1,85 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
 using namespace std;
 int n,m;
-queue<int> 
+queue<int> q;
+bool visit[1000];
 string person[1000];
-vector<int> graph[1000];
+unordered_map<string, int> dict;
+vector<int> tree[1000];
+vector<int> root;
+vector<int> node[1000];
+vector<int> rnode[1000];
 int deg[1000];
-int getindex(string s){
-    for(int i=0;i<n;i++){
-        if(s == person[i]) return i;
+// int getindex(string s){
+//     for(int i=0;i<n;i++){
+//         if(s == person[i]) return i;
+//     }
+//     return -1;
+// }
+void bfs(){
+    while(!q.empty()){
+        int f = q.front();
+        q.pop();
+        for(int i=0;i<node[f].size();i++){
+            int a = node[f][i];
+            if(!visit[a]){
+                visit[a] = true;
+                tree[f].push_back(a);
+            }
+        }
+        for(int i=0;i<rnode[f].size();i++){
+            int a = rnode[f][i];
+            deg[a]--;
+            if(deg[a] == 0) q.push(a);
+        }
     }
-    return -1;
 }
 int main(){
-
     fill_n(deg,1000,0);
     cin >> n;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
         cin >> person[i];
-    }
+    sort(person, person+n);
+    for(int i=0;i<n;i++)
+        dict[person[i]] = i;
     cin >> m;
     for(int i=0;i<m;i++){
         string tmp1, tmp2;
         cin >> tmp1 >> tmp2;
-        int a = getindex(tmp1);
-        int b = getindex(tmp2);
-        graph[a].push_back(b);
-        deg[a]++;
+        int a = dict[tmp1];
+        int b = dict[tmp2];
+        deg[b]++;
+        node[b].push_back(a);
+        rnode[a].push_back(b);
     }
     for(int i=0;i<n;i++){
         if(deg[i] == 0) {
             q.push(i);
         }
+        if(rnode[i].empty()) root.push_back(i);
     }
-    printf("%d", q.size());
-    for(int i=0;i<q.size();i++){
-        
+    bfs();
+    cout << root.size() << endl;
+    for(int i=0;i<root.size();i++){
+        cout << person[root[i]] << ' ';
+    }
+    cout << endl;
+    for(int i=0;i<n;i++){
+        cout << person[i] << ' ' << tree[i].size();
+        vector<string> child;
+        for(int j=0;j<tree[i].size();j++){
+            child.push_back(person[tree[i][j]]);
+        }
+        sort(child.begin(),child.end());
+        for(int j=0;j<tree[i].size();j++){
+            cout << ' ' << child[j];
+        }
+        cout << '\n';
     }
     return 0;
 }
