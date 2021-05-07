348f3b87d10797d5e332ffd8e5d2af5df55b97ca
Jaeyong <ggjae_@daum.net>
2021-05-07 17:07:49 +0900

    programmers queue

---
+++ cpp/pgm/printer2.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    vector<int> v;
+    queue<pair<int, int> > q;
+    for(int i=0;i<priorities.size();i++){
+        q.push({i,priorities[i]});
+    }
+    sort(priorities.begin(),priorities.end(),greater<>());
+    int idx = 0;
+    while(!q.empty()){
+        int index = q.front().first;
+        int priority = q.front().second;
+        q.pop();
+        if(priority == priorities[idx]){
+            idx++;
+            v.push_back(index);
+        } else {
+            q.push({index, priority});
+        }
+    }
+    for(int i=0;i<v.size();i++){
+        printf("v[i]:%d\n",v[i]);
+        // if(location == v[i]){
+        //     answer = i;
+        //     break;
+        // }
+    }
+    return answer;
+}
+int main(){
+    solution({2,1,3,2},2);
+    return 0;
+}
