fe6c50e94d33e91e4c90a34403e738dbd8fd8436
Jaeyong <ggjae_@daum.net>
2021-07-29 18:55:43 +0900

    algstudy cpp

--- cpp/3649.cpp
+++ cpp/3649.cpp
@@ -4,37 +4,36 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int sum = 0;
-    bool finish=false;
-    int num = 0;
-    int val = 0;
-    int l = 0;
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     vector<int> v;
-    int len = 0;
-    cin >> len >> num;
-    len*=10000000;
-    v.clear();
-    for(int i=0;i<num;i++){
-        cin>> val;
-        v.push_back(val);   
-    }
-    sort(v.begin(),v.end());
-    int r = v.size()-1;
-    while(l<r){
-        sum = v[l]+v[r];
-        if(sum==len){
-            finish = true;
-            cout << "yes "<< v[l] << " " << v[r] << '\n';
-            break;
+    int len;
+    while (1){
+        if(cin>>len){
+            len*=10000000;
+            int num,val;
+            cin>>num;
+            v.clear();
+            for(int i=0;i<num;i++){
+                cin>> val;
+                v.push_back(val);   
+            }
+            sort(v.begin(),v.end());
+            int l=0,r=v.size()-1,sum;
+            bool finish=false;
+            while(l<r){
+                sum = v[l]+v[r];
+                if(sum==len){
+                    finish = true;
+                    cout << "yes "<< v[l]<<" "<<v[r]<<'\n';
+                    break;
+                }
+                else if(sum < len)  l++;
+                else r--;
+            }
+            if(!finish)
+                cout << "danger\n";
         }
-        else if(sum < len)  l++;
-        else r--;
-    }
-    if(!finish){
-        cout << "danger\n";
+        else break;
     }
     return 0;
 }
